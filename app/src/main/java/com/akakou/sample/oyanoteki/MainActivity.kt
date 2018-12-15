3436a13a0f0b2867f7b4a5102fe8d7e618de977f
akakou <s16003@tokyo.kosen-ac.jp>
2018-12-15 17:09:29 +0900

    version 1完成

--- app/src/main/java/com/akakou/sample/oyanoteki/MainActivity.kt
+++ app/src/main/java/com/akakou/sample/oyanoteki/MainActivity.kt
@@ -1,28 +1,21 @@
 package com.akakou.sample.oyanoteki
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import kotlinx.coroutines.experimental.android.UI
-import kotlinx.coroutines.experimental.launch
-import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        thread {
-            while(true) {
-                UDP.send("hello")
-                val data = UDP.receive()
+    override fun onPause() {
+        super.onPause()
 
-                launch(UI) {
-                    Toast.makeText(this@MainActivity, "$data", Toast.LENGTH_SHORT).show()
-                }
-            }
-        }
+        val intent = Intent(this, MainService::class.java)
+        startService(intent)
     }
 }
--- app/src/main/java/com/akakou/sample/oyanoteki/UDP.kt
+++ app/src/main/java/com/akakou/sample/oyanoteki/UDP.kt
@@ -3,33 +3,27 @@ package com.akakou.sample.oyanoteki
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import java.io.IOException
 
 
 object UDP {
     var ip = InetAddress.getByName("192.168.43.255")
     var port = 5000
 
-    fun receive() : Char {
-        // udpを受信して、文字列にして返す
+    fun receive() : Byte {
         val socket = DatagramSocket(port)
+        val buffer = ByteArray(1024)
+        val packet = DatagramPacket(buffer, 0, buffer.size)
 
-        val buffer = ByteArray(1)
-        val packet = DatagramPacket(buffer, buffer.size)
+        val result = try {
+            socket.receive(packet)
+            buffer[0]
+        } catch (e: IOException) {
+            0.toByte()
+        }
 
-        socket.receive(packet)
         socket.close()
 
-        return String(buffer)[0]
-    }
-
-    fun send(msg: String) {
-        // udpで文字列を送信する
-        val socket = DatagramSocket(port)
-
-        val byte = msg.toByteArray();
-        val packet = DatagramPacket(byte, byte.size, ip, port);
-
-        socket.send(packet);
-        socket.close()
+        return result
     }
 }
