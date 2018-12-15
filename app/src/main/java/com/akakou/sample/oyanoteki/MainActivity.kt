74af228b1aa88dbbd3393396a7d05f2da4c579e9
akakou <s16003@tokyo.kosen-ac.jp>
2018-12-15 13:13:17 +0900

    プロジェクトの作成

---
+++ app/src/main/java/com/akakou/sample/oyanoteki/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.akakou.sample.oyanoteki
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
